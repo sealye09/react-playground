5c4411b3727812a43425ae9eae56cf5aabadc3b2
SEAL <yejunhai2001@gmail.com>
2023-08-16 14:15:03 +0800

    add: 给toast组件添加位置variant

--- src/components/Toast/ToastContainer.tsx
+++ src/components/Toast/ToastContainer.tsx
@@ -1,19 +1,37 @@
 import { FC, useContext } from "react";
+import { VariantProps, cva } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
 import { ToastContext, ToastPosition } from ".";
 import Toast from "./Toast";
 
-interface ToastContainerProps {
+const positionVariant = cva(["absolute z-50 space-y-4 min-w-fit"], {
+  variants: {
+    position: {
+      "top-left": "top-8 left-12",
+      "top-center": "top-8 left-1/2 -translate-x-1/2",
+      "top-right": "top-8 right-12",
+      "bottom-left": "bottom-8 left-12",
+      "bottom-center": "bottom-8 left-1/2 -translate-x-1/2",
+      "bottom-right": "bottom-8 right-12",
+    },
+  },
+  defaultVariants: {
+    position: "top-center",
+  },
+});
+interface ToastContainerProps extends VariantProps<typeof positionVariant> {
   position?: ToastPosition;
   duration?: number;
 }
 
 export const ToastContainer: FC<ToastContainerProps> = ({
-  duration = 3000,
   position = "top-center",
+  duration = 3000,
 }) => {
   const { state } = useContext(ToastContext);
   return (
-    <div className="absolute top-4 z-50 left-1/2 -translate-x-1/2 space-y-4 min-w-fit">
+    <div className={twMerge(positionVariant({ position }))}>
       {!!state &&
         state.map((toast) => {
           return (
